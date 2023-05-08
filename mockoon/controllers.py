c2cca15cb8bb28a4e8f2cccab5fff1995879dd84
m-roberts <2947595+m-roberts@users.noreply.github.com>
2023-05-08 03:03:50 +0100

    Use pre-commit in build pipeline

--- mockoon/controllers.py
+++ mockoon/controllers.py
@@ -5,93 +5,93 @@ from .server import MockoonServer
 
 
 class ServerController(ABC):
-  @abstractmethod
-  def start(self):
-    ...
+    @abstractmethod
+    def start(self):
+        ...
 
-  @abstractmethod
-  def stop(self):
-    ...
+    @abstractmethod
+    def stop(self):
+        ...
 
 
 class MockoonServerController(ServerController):
-  def __init__(self, server: MockoonServer):
-    self.server = server
+    def __init__(self, server: MockoonServer):
+        self.server = server
 
-  def start(self):
-    self.server.start()
+    def start(self):
+        self.server.start()
 
-  def stop(self):
-    self.server.stop()
+    def stop(self):
+        self.server.stop()
 
 
 class TransactionController(ABC):
-  @property
-  @abstractmethod
-  def transactions(self) -> list[Transaction]:
-    ...
+    @property
+    @abstractmethod
+    def transactions(self) -> list[Transaction]:
+        ...
 
-  @abstractmethod
-  def reset_transactions(self):
-    ...
+    @abstractmethod
+    def reset_transactions(self):
+        ...
 
 
 class MockoonTransactionController(TransactionController):
-  def __init__(self, server: MockoonServer):
-    self.server = server
+    def __init__(self, server: MockoonServer):
+        self.server = server
 
-  @property
-  def transactions(self) -> list[Transaction]:
-    return self.server.transactions
+    @property
+    def transactions(self) -> list[Transaction]:
+        return self.server.transactions
 
-  def reset_transactions(self):
-    self.server.reset_transactions()
+    def reset_transactions(self):
+        self.server.reset_transactions()
 
 
 class LoggingController(ABC):
-  @abstractmethod
-  def start_logging(self):
-    ...
+    @abstractmethod
+    def start_logging(self):
+        ...
 
-  @abstractmethod
-  def stop_logging(self):
-    ...
+    @abstractmethod
+    def stop_logging(self):
+        ...
 
 
 class HttpTransactionController(ABC):
-  @abstractmethod
-  def wait_for_route_hit(self, route: str):
-    ...
+    @abstractmethod
+    def wait_for_route_hit(self, route: str):
+        ...
 
 
 class StateController(ABC):
-  @abstractmethod
-  def wait_for_active(self):
-    ...
+    @abstractmethod
+    def wait_for_active(self):
+        ...
 
 
 class MockoonLoggingController(LoggingController):
-  def __init__(self, server: MockoonServer):
-    self.server = server
+    def __init__(self, server: MockoonServer):
+        self.server = server
 
-  def start_logging(self):
-    self.server.start_log_stream()
+    def start_logging(self):
+        self.server.start_log_stream()
 
-  def stop_logging(self):
-    self.server.stop_log_stream()
+    def stop_logging(self):
+        self.server.stop_log_stream()
 
 
 class MockoonHttpTransactionController(HttpTransactionController):
-  def __init__(self, server: MockoonServer):
-    self.server = server
+    def __init__(self, server: MockoonServer):
+        self.server = server
 
-  def wait_for_route_hit(self, route: str):
-    self.server.wait_for_route_hit(route)
+    def wait_for_route_hit(self, route: str):
+        self.server.wait_for_route_hit(route)
 
 
 class MockoonStateController(StateController):
-  def __init__(self, server: MockoonServer):
-    self.server = server
+    def __init__(self, server: MockoonServer):
+        self.server = server
 
-  def wait_for_active(self):
-    self.server.wait_for_active()
+    def wait_for_active(self):
+        self.server.wait_for_active()
